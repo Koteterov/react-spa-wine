3fc576285ca284581675bebd125ba041c300ef37
Dimitar Koteterov <koteterov@yahoo.com>
2023-03-05 15:19:23 +0200

    static home page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </main>
       <Footer />
     </>
   );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,35 @@
+import styles from "./Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <section id="home">
+        <div className={styles["home-container"]}>
+          <div className={styles["short-info"]}>
+            <h1>The best of the wine selection.</h1>
+            <h2>
+              Share your choice to add value to the wine market in the country.
+            </h2>
+          </div>
+        </div>
+      </section>
+
+      <section id="home-page">
+        <div className={styles["offers"]}>
+          <div className={styles["container"]}>
+            <img src="./images/bottle-neck.jpg" alt="bottle-neck" />
+          </div>
+          <div className={styles["container"]}>
+            <img src="./images/wine-drink.jpg" alt="wine-drink" />
+          </div>
+          <div className={styles["container"]}>
+            <img src="./images/red-wine-glass-bottle.jpg" alt="red-wine" />
+          </div>
+          <div className={styles["container"]}>
+            <img src="./images/red-wine-bottles.jpg" alt="red-wine-bottles" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
